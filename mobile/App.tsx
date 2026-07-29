f17f33e37af28f9afed821c33a5752accb637178
Jonathan <jonathanrubens10@gmail.com>
2026-07-29 12:50:15 -0300

    Instalando as depedencias a ajustando a dashboard para tela inicial

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import BottomMenu from "./src/components/menu/menu";
-import * as NavigationBar from "expo-navigation-bar";
+import Dashboard from "./src/screens/Dashboard/App";
 
 export default function App() {
-  useEffect(() => {
-    NavigationBar.setVisibilityAsync("hidden");
-  }, []);
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <BottomMenu />
-    </View>
-  );
+  return <Dashboard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
