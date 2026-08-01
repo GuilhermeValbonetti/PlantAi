7b29a8791c83896cdffb133318d75c3a91e4005d
unknown <yallisonregis@gmail.com>
2026-08-01 15:35:32 -0300

    Criação da tabela de consulta e criação do arquivo de conexão com o banco

--- database/plantai.sql
+++ database/plantai.sql
@@ -10,3 +10,12 @@ CREATE TABLE usuario (
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL
 );
+CREATE TABLE consulta (id SERIAL PRIMARY KEY, 
+foto VARCHAR(200),
+nome_planta VARCHAR(100),
+analise VARCHAR(500),
+grau VARCHAR(30),
+doenca VARCHAR(50),
+probabilidade INT,
+data TIMESTAMP,
+id_usuario SERIAL REFERENCES usuario(id))
